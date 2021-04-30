02980f0d1e23b9c15ecfe301f9801a4257055e56
Bowtie <82338303+bowtie-ltsa@users.noreply.github.com>
2021-04-29 18:08:53 -0700

    move createCookie fn; delete dead code.

--- php/set-volunteer.php
+++ php/set-volunteer.php
@@ -1,7 +1,7 @@
 <?php
     declare(strict_types=1);
     require_once "first-things.php";
-    require_once "createCookie.php";
+    require_once "functions/createCookie.php";
 
     include "get-volunteer.php";
 
